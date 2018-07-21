4465a6c9b7feca3fefd5b47131f329d710c90460
serene choo <choomls@gmail.com>
2018-07-21 09:34:24 +0800

    Did a lot of stuff

--- Cliker/ViewController.swift
+++ Cliker/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var score: UILabel!
+    var counter = 0
+    var time = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func Clicked(_ sender: Any) {
+        counter += 1
+        score.text = String(counter)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
